6e41663548472d63c97b23f40dda603a0d6dc26a
Chris Doyle <cdoyle@temple.edu>
2020-11-17 16:57:28 -0500

    subjects working Finish reviews service with tests. Initial feeds and tests complete.

--- spec/services/xml_catalogs_spec.rb
+++ spec/services/xml_catalogs_spec.rb
@@ -5,37 +5,36 @@ require "rails_helper"
 RSpec.describe SyncService::Catalogs, type: :service do
 
   before(:all) do
-    @catalog_harvest = described_class.new(xml_path: file_fixture("books.xml").to_path)
+    @catalog_harvest = described_class.new(xml_path: file_fixture("delta.xml").to_path)
     @catalogs = @catalog_harvest.read_catalogs
   end
 
   context "valid catalogs" do
     it "extracts the catalog hash" do
-      expect(@catalogs.first["catalog"]).to match(/^SP06$/)
+      expect(@catalogs.first["catalog"]).to match(/^FA21$/)
     end
 
     it "extracts all of the catalogs" do
-      expect(@catalogs.count).to equal(2)
+      expect(@catalogs.count).to equal(6)
     end
 
     describe "maps catalogs xml to db schema" do
       subject { @catalog_harvest.record_hash(@catalogs.first) }
 
       it "maps Code to code field" do
-        # binding.pry
         expect(subject["code"]).to match(@catalogs.first["catalog"])
       end
 
       it "maps title to title field" do
-        expect(subject["title"]).to match("Spring 2006 Catalog")
+        expect(subject["title"]).to match("Fall 2021 Catalog")
       end
 
       it "maps season to season field" do
-        expect(subject["season"]).to match("Spring")
+        expect(subject["season"]).to match("Fall")
       end
 
       it "maps year to year field" do
-        expect(subject["year"]).to match("2006")
+        expect(subject["year"]).to match("2021")
       end
     end
   end
@@ -46,11 +45,11 @@ RSpec.describe SyncService::Catalogs, type: :service do
     end
 
     let (:catalog1) {
-      Catalog.find_by(code: "SP06")
+      Catalog.find_by(code: "FA21")
     }
 
     let (:catalog2) {
-      Catalog.find_by(code: "FA05")
+      Catalog.find_by(code: "SP21")
     }
 
     it "syncs catalogs to the table" do
