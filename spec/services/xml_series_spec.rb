a93c0852160e80ab0cb3a2c577fbd62c36b040da
Chris Doyle <cdoyle@temple.edu>
2020-12-18 11:46:28 -0500

    Add tabs to book edit form.

--- spec/services/xml_series_spec.rb
+++ spec/services/xml_series_spec.rb
@@ -11,8 +11,7 @@ RSpec.describe SyncService::Series, type: :service do
 
   context "valid series" do
     it "extracts the series hash" do
-      # expect(@series.second["series"]["series_id"]).to match(/^S-911$/)
-      expect(@series.first.children.first.text).to match(/^S-196$/)
+      expect(@series.first["series"]["series_id"]).to match(/^S-196$/)
     end
 
     it "extracts all of the series" do
