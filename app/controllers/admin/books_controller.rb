229ca5a147e1c34879938d0fcd077f60dbe6d30a
Chris Doyle <cdoyle@temple.edu>
2021-01-27 16:57:30 -0500

    rework detachable concern for multiple image possibilities (books only for now). Populate subjects fields. Add image gems.

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -3,6 +3,22 @@
 module Admin
   class BooksController < Admin::ApplicationController
     include Admin::Detachable
+    before_action :set_tabs, only: [:edit]
+    #before_action :set_fields, only: [:edit]
+
+    def set_tabs
+      @tab_names = I18n.t("tupress.admin.tabs.books")
+      @books_tab = ["sort_title","cover_image","status","excerpt_image","excerpt_text"]
+    end
+
+    def set_fields
+        resource_class.attribute_names.each do |att|
+          if @books_tab.include?(att)
+            binding.pry
+          end
+        end
+
+    end
 
     def default_sort
       { order: :title, direction: :asc }
