33249be329b50a2b8ff2b96f862b171d1eb4fd3e
Chris Doyle <chris@gcdoyle.com>
2021-02-02 14:12:19 -0500

    Cleanup migrations

--- db/migrate/20210202132517_add_file_field_label_to_subjects.rb
+++
@@ -1,5 +0,0 @@
-class AddFileFieldLabelToSubjects < ActiveRecord::Migration[6.0]
-  def change
-    add_column :subjects, :file_label, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_132517) do
+ActiveRecord::Schema.define(version: 2021_02_02_141624) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,24 @@ ActiveRecord::Schema.define(version: 2021_02_02_132517) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "agencies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.string "contact"
+    t.string "address1"
+    t.string "address2"
+    t.string "address3"
+    t.string "city"
+    t.string "country"
+    t.string "phone"
+    t.string "fax"
+    t.string "email"
+    t.string "website"
+    t.string "region"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "rights"
   end
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
