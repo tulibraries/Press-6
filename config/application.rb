4d984ed1384f7288fce79a6877f52fb33ff32404
Brian Hoffman <brian.hoffman0003@temple.edu>
2021-02-04 10:51:11 -0500

    move aws client config to initializer and skip unless storage service is :amazon

--- config/application.rb
+++ config/application.rb
@@ -32,18 +32,3 @@ module Tupress
     # the framework and any gems in your application.
   end
 end
-
-sts = Aws::STS::Client.new(
-  access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-  secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-  region: "us-east-1"
-)
-
-
-Aws.config.update({
-   credentials: Aws::AssumeRoleCredentials.new(
-     client: sts,
-     role_arn: Rails.application.credentials.dig(:aws, :role_arn),
-     role_session_name: "session-name",
-     region: "us-east-1"
-   ) })
