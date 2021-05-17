9ac5786be18fce22882a30c5685d53a4751ffa9a
Chris Doyle <chris@gcdoyle.com>
2021-05-17 15:06:31 -0400

    add related links

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,12 @@ class BooksController < ApplicationController
     @series = Series.find_by(code: @book.series_id)
     @awards = [@book.award, @book.award2, @book.award3]
     @subjects = [Subject.find_by(code: @book.subject1), Subject.find_by(code: @book.subject2), Subject.find_by(code: @book.subject3)].compact
+    @links = [
+                link: { label: @book.label_1, link: @book.link_1 }, 
+                link: { label: @book.label_2, link: @book.link_2 }, 
+                link: { label: @book.label_3, link: @book.link_3 }  
+              ]
+    # @links = [[@book.label_1, @book.link_1],[@book.label_2, @book.link_2],[@book.label_3, @book.link_3]]
   end
 
   private
