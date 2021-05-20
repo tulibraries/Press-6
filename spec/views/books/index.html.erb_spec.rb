e2b0587edf5c9522a87fd9202f990e1625ad8914
Chris Doyle <chris@gcdoyle.com>
2021-05-20 10:54:50 -0400

    add tests

---
+++ spec/views/books/index.html.erb_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "books/index.html.erb", type: :view do
+  let(:book) { FactoryBot.create(:book, title: "A Formation") }
+
+  before(:each) do
+    assign(:books, Kaminari.paginate_array([
+      book
+    ]).page(1))
+  end
+
+  it "populates paginated list" do
+    render
+    expect(rendered).to match /#{book.title}/
+  end  
+end
