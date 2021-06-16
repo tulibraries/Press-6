1870d38a2d7002253fca11c10086a8c895ae6501
Chris Doyle <chris@gcdoyle.com>
2021-06-16 12:54:55 -0400

    Update feature test for CI empty db

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -3,17 +3,23 @@
  require "rails_helper"
 
  RSpec.describe "/people", type: :feature do
-  let(:dept_head) { FactoryBot.create(:person, name: "Not a Lemon Head", department: "Human Resources", head: true) }
-  let(:default_person) { FactoryBot.create(:person, name: "Lemon Head", department: "Human Resources", head: false) }
-  let(:person) { FactoryBot.create(:person, :with_image) }
-
   it "displays all people" do
+    dept_head = FactoryBot.create(:person, name: "Not a Lemon Head", department: "Human Resources", head: true)
+    default_person = FactoryBot.create(:person, name: "Lemon Head", department: "Human Resources", head: false)
+    person = FactoryBot.create(:person, :with_image)
     visit people_path
-    expect(page.body).to match /Moto Mori/
-    expect(page.body).to match /Lemon Head/
+    container = page.find("#people")
+    within(container) do
+      expect(page.body).to match /Moto Mori/
+      expect(page.body).to match /Lemon Head/
+      expect(page.body).to match /Not a Lemon Head/
+    end
   end
 
   it "returns department head first" do
+    dept_head = FactoryBot.create(:person, name: "Not a Lemon Head", department: "Human Resources", head: true)
+    default_person = FactoryBot.create(:person, name: "Lemon Head", department: "Human Resources", head: false)
+    person = FactoryBot.create(:person, :with_image)
     visit people_path
     container = page.find("#people")
     within(container) do
