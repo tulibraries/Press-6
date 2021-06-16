10f37df9c7204a91d7ae8fe68715d004bf8195d1
Chris Doyle <chris@gcdoyle.com>
2021-06-16 13:21:28 -0400

    Update test again

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -4,8 +4,8 @@
 
  RSpec.describe "/people", type: :feature do
   it "displays all people" do
-    dept_head = FactoryBot.create(:person, name: "Not a Lemon Head", department: "Human Resources", head: true)
-    default_person = FactoryBot.create(:person, name: "Lemon Head", department: "Human Resources", head: false)
+    dept_head = FactoryBot.create(:person, :with_image, name: "Not a Lemon Head", department: "Human Resources", head: true)
+    default_person = FactoryBot.create(:person, :with_image, name: "Lemon Head", department: "Human Resources", head: false)
     person = FactoryBot.create(:person, :with_image)
     visit people_path
     container = page.find("#people")
@@ -17,8 +17,8 @@
   end
 
   it "returns department head first" do
-    dept_head = FactoryBot.create(:person, name: "Not a Lemon Head", department: "Human Resources", head: true)
-    default_person = FactoryBot.create(:person, name: "Lemon Head", department: "Human Resources", head: false)
+    dept_head = FactoryBot.create(:person, :with_image, name: "Not a Lemon Head", department: "Human Resources", head: true)
+    default_person = FactoryBot.create(:person, :with_image, name: "Lemon Head", department: "Human Resources", head: false)
     person = FactoryBot.create(:person, :with_image)
     visit people_path
     container = page.find("#people")
