4809a1313665d20f5da0818eb46a8026c4476b09
Chris Doyle <chris@gcdoyle.com>
2021-06-16 11:31:47 -0400

    fix failing test

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -3,23 +3,21 @@
  require "rails_helper"
 
  RSpec.describe "/people", type: :feature do
-   let(:default_person) { FactoryBot.create(:person, name: "A Lemon Head", department: "Human Resources") }
-   let(:dept_head) { FactoryBot.create(:person, name: "Not a Lemon Head", department: "Human Resources", head: true) }
-   let(:person) { FactoryBot.create(:person, :with_image) }
+  let(:dept_head) { FactoryBot.create(:person, name: "Not a Lemon Head", department: "Human Resources", head: true) }
+  let(:default_person) { FactoryBot.create(:person, name: "Lemon Head", department: "Human Resources", head: false) }
+  let(:person) { FactoryBot.create(:person, :with_image) }
 
-   describe "index page" do
-    it "displays all people" do
-      visit people_path
-      expect(page.body).to match /John/
-      expect(page.body).to match /Lemon Head/
-    end
+  it "displays all people" do
+    visit people_path
+    expect(page.body).to match /Moto Mori/
+    expect(page.body).to match /Lemon Head/
+  end
 
-    it "returns department head first" do
-      visit people_path
-      container = page.find("#people")
-      within(container) do
-        expect(default_person.name).to appear_before(dept_head.name, only_text: true)
-      end
+  it "returns department head first" do
+    visit people_path
+    container = page.find("#people")
+    within(container) do
+      expect(dept_head.name).to appear_before(default_person.name, only_text: true)
     end
   end
  end
