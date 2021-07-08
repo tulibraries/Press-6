f6ff5e4d4922569014b65bd56bb1776d33dac2ad
Chris Doyle <chris@gcdoyle.com>
2021-07-08 10:47:32 -0400

    Add century routine

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -4,10 +4,15 @@ class CatalogsController < ApplicationController
   before_action :set_catalog, only: %i[show]
 
   def index
-    @catalogs = Catalog.all
+    @catalogs = Catalog.where("suppress != ?", "").order(created_at: :desc)
+    @show_status = ["NP", "IP","OS","OP"]
   end
 
   def show
+    @catalog = Catalog.find_by code: "#{params[:code]}"
+    @books = Book.where('catalog = ?', "#{params[:code]}").where({ status: ["NP","IP","OS","OP"] }).order(:sort_title)
+    @show_status = ["NP", "IP","OS","OP"]    
+    @brochures = Brochure.where(catalog_code: params[:code]).where(promoted_to_subject: true)
   end
 
   private
