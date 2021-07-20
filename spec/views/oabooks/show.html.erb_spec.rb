4b2b8efe601ac6c9ba257f47fe660fe18dc7b54c
Chris Doyle <chris@gcdoyle.com>
2021-07-20 17:42:14 -0400

    fix test

--- spec/views/oabooks/show.html.erb_spec.rb
+++ spec/views/oabooks/show.html.erb_spec.rb
@@ -3,7 +3,12 @@
 require "rails_helper"
 
 RSpec.describe "oabooks/show.html.erb", type: :view do
+  let(:user) { FactoryBot.create(:user) }
 
+  before(:each) do
+    allow(view).to receive(:current_user).and_return(user)
+  end
+  
   it "uses helper to create manifold link" do
     @oabook = FactoryBot.create(:oabook)
     render
