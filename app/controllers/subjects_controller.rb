7b95275f3f3cb6b149046fe303b3a90654b62905
Chris Doyle <chris@gcdoyle.com>
2021-07-27 14:52:13 -0400

    Remove unnecessary controller method

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -14,15 +14,5 @@ class SubjectsController < ApplicationController
     def set_subject
       @subject = Subject.find(params[:id])
     end
-
-    def get_subjects(tuples)
-      @subjects = []
-      tuples.each do |subject|
-        subject.each do |s|
-          @subjects << s
-        end
-        @subjects.reject(&:blank?)
-      end
-      @subjects.uniq.sort_by { |h| h[0] }
-    end
+    
 end
