18e9c3afba469b4ed983736221f01f7a24d06361
Chris Doyle <chris@gcdoyle.com>
2021-07-29 12:12:55 -0400

    wip

--- app/services/sync_service/books.rb
+++ app/services/sync_service/books.rb
@@ -24,6 +24,8 @@ class SyncService::Books
         record = record_hash(book)
         create_or_update_if_needed!(record)
       rescue Exception => err
+
+        
         stdout_and_log(%Q(Syncing Book: #{book["title"]} errored -  #{err.message} \n #{err.backtrace}))
         @errored += 1
       end
@@ -43,8 +45,15 @@ class SyncService::Books
       "title"               => record.dig("record", "title"),
       "subtitle"            => record.dig("record", "subtitle"),
       "cover"               => record.dig("record", "cover_image").sub("http://www.temple.edu/tempress/titles/", ""),
-      "author_ids"          => record["record"].fetch("authors")["author"].map do |p|
-                                  p.kind_of?(Hash) ? p["author_id"] : p[1]
+      "author_ids"          => record["record"].fetch("authors")["author"].filter do |p|
+                                if p.kind_of?(Array)
+                                  binding.pry
+                                  p[0] == "author_id" ? p[1] : p[1]
+                                end
+                                if p.kind_of?(Hash)
+                                  binding.pry
+                                  p["author_id"]
+                                end
                                 end,
       "author_byline"       => record.dig("record", "author_byline"),
       "about_author"        => record.dig("record", "author_bios"),
