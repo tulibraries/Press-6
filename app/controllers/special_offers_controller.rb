3346145cacfb5524436111eec964bc81536930e5
Chris Doyle <chris@gcdoyle.com>
2021-08-02 18:28:29 -0400

    model, controller, index page

--- app/controllers/special_offers_controller.rb
+++ app/controllers/special_offers_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class SpecialOffersController < ApplicationController
-  before_action :set_special_offer, only: %i[show]
+  before_action :set_special_offer, only: :show
 
   def index
-    @special_offers = SpecialOffer.all
+    @special_offers = SpecialOffer.all.where(active: true)
   end
 
   def show
