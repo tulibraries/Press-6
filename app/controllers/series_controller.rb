172adc74a4e83f1979f0b0faeddf8613357d77bc
Chris Doyle <chris@gcdoyle.com>
2021-08-02 12:35:52 -0400

    Add index and show views. Add books index to series Update book titles to use html Update css and div tag on index views

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,17 +1,21 @@
 # frozen_string_literal: true
 
 class SeriesController < ApplicationController
-  before_action :set_series, only: %i[show]
+  before_action :set_series, only: :show
 
   def index
-    @series = Series.all
+    @series = Series.all.order(:title)
   end
 
   def show
+    @books = @series.books
+                    .where(status: show_status)
+                    .order(:sort_title)
+                    .page params[:page]
   end
 
   private
     def set_series
-      @series = Series.find(params[:id])
+      @series = Series.find_by(code: params[:id])
     end
 end
