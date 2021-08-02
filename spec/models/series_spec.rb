482099c2029e3ae35d2076b7954ffbe6197d00b0
Chris Doyle <chris@gcdoyle.com>
2021-08-02 14:06:33 -0400

    add optional image to series page and update tests

---
+++ spec/models/series_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Series, type: :model do
+  describe "validations" do
+    it { should validate_presence_of(:code) }
+    it { should validate_presence_of(:title) }
+  end
+
+  describe "associations" do
+    it { should have_many(:books) }
+  end
+
+  it_behaves_like "attachable"
+  it_behaves_like "detachable"
+end
