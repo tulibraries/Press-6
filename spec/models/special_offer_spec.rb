bcf81358d0fc482d8b9dd802bdece42b0752f7c8
Chris Doyle <chris@gcdoyle.com>
2021-08-03 09:15:41 -0400

    add show page and tests

--- spec/models/special_offer_spec.rb
+++ spec/models/special_offer_spec.rb
@@ -3,6 +3,9 @@
 require "rails_helper"
 
 RSpec.describe SpecialOffer, type: :model do
+  describe "associations" do
+    it { should have_many(:books) }
+  end
 
   it_behaves_like "attachable"
   it_behaves_like "detachable"
