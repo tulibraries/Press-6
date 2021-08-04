dc6b488f71f5e4390a9556bbc2816a1621b47b30
Chris Doyle <chris@gcdoyle.com>
2021-08-04 11:43:44 -0400

    Clean up code

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,6 +3,5 @@
 class DocumentsController < ApplicationController
   def index
     @documents_by_type = Document.all.group_by { |t| t.document_type }
-    # @documents_by_contact = @documents_by_type.map { |type, documents| binding.pry }
   end
 end
