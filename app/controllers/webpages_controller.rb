ae53e3c97dae8f81808c75fc9c592893736c3bf1
Chris Doyle <chris@gcdoyle.com>
2021-09-16 15:11:22 -0400

    Add new and forthcoming section

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -8,6 +8,8 @@ class WebpagesController < ApplicationController
     @highlights = Highlight.where(promote_to_homepage: true).sort
     @featured_catalog = Catalog.where.not(suppress: 1).order([year: :desc, season: :asc]).first
     @brochure = Brochure.where(promoted_to_homepage: 1).order(updated_at: :desc).first
+    @philly = Subject.find_by(title: "Philadelphia Region")
+    @hot = Book.where(hot: true).order(updated_at: :desc).take(3).sort
   end
 
   def show
--- app/models/webpage.rb
+++ app/models/webpage.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Webpage < ApplicationRecord
+  include Imageable
+
   has_rich_text :body
   validates :title, presence: true
 end
