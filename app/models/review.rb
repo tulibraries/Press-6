89da3439022fae2a3f52a910bb201b3f3a0bb00f
Chris Doyle <chris@gcdoyle.com>
2021-09-27 14:02:31 -0400

    fix inverse relation on books and reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
-  include Friendable
   validates :review, :review_id, presence: true
-  has_one :book, class_name: "Book", foreign_key: "book_id", dependent: :destroy, inverse_of: :review
+  has_one :book, class_name: "Book", foreign_key: "book_id", dependent: :destroy, inverse_of: :books
 end
