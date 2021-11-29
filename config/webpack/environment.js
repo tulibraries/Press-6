9133af4869f80836ad51c72a86ec9ae2681661c3
Chris Doyle <chris@gcdoyle.com>
2021-11-29 16:12:58 -0500

    remove package-lock.json; update binstubs; remove coffee config; ruby 2.7.5

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const coffee =  require('./loaders/coffee')
-
 const webpack = require('webpack')
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
@@ -9,5 +7,4 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Popper: ['popper.js', 'default']
 }))
 
-environment.loaders.prepend('coffee', coffee)
 module.exports = environment
