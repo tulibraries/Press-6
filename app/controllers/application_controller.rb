3f3564719519e215ddde537b50bf412b7d28cbfa
Chris Doyle <chris@gcdoyle.com>
2021-12-06 12:31:14 -0500

    Updates main menu to link to imported data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :get_journals
-
-  def get_journals
-    @journals = Journal.all
-  end
+  before_action :set_menu
 
   def show_status
     ["NP", "IP", "OP"]
+  end
+
+  def set_menu
+    @journals = Journal.all
+    @ordering_information = Webpage.friendly.find("ordering-information")
+    @submissions = Webpage.friendly.find("submissions")
+    @about = Webpage.friendly.find("about-the-press")
+    @about_u_presses = Webpage.friendly.find("about-university-presses")
+    @payment_terms = Webpage.friendly.find("payment-terms")
   end
 end
