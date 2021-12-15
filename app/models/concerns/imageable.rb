00ec1254a8d0af72b0018f83317f42d8a9de7e49
Chris Doyle <cdoyle@temple.edu>
2021-12-15 16:01:15 -0500

    #TWM-285 cover images (#237)
    * remove lib/uploads; refactor custom_image
    
    * Analyze blog before calcualtions
    
    * rails generated bin files
    
    Co-authored-by: Chris Doyle <chris@gcdoyle.com>

--- app/models/concerns/imageable.rb
+++ app/models/concerns/imageable.rb
@@ -2,7 +2,6 @@
 
 module Imageable
   extend ActiveSupport::Concern
-  require Rails.root.join("lib/uploads.rb")
 
   def thumb_image(image_field)
     custom_image(image_field, 160, 220)
@@ -17,20 +16,23 @@ module Imageable
   end
 
   def custom_image(image_field, width, height)
-    if ((self.send(image_field.to_sym).blob.metadata[:width] != width) || (self.send(image_field.to_sym).blob.metadata[:height] != height))
-      self.send(image_field.to_sym).variant(image_variation(image_field, width, height)).processed
+    self.send(image_field.to_sym).blob.analyze
+    if (self.send(image_field.to_sym).blob.metadata[:width] != width) || (self.send(image_field.to_sym).blob.metadata[:height] != height)
+      if self.send(image_field.to_sym).blob.metadata[:width] > self.send(image_field.to_sym).blob.metadata[:height]
+        self.send(image_field.to_sym).variant(format: :png,
+                      background: :transparent,
+                      gravity: "North",
+                      resize_to_fit: [width, height]
+                    ).processed
+      else
+        self.send(image_field.to_sym).variant(format: :png,
+          background: :transparent,
+          gravity: :center,
+          resize_and_pad: [width, height]
+        ).processed
+      end
     else
       self.send(image_field.to_sym)
-    end if image_field.present?
-  end
-
-  def image_variation(image_field, width, height)
-    self.send(image_field.to_sym).blob.analyze
-
-    if self.send(image_field.to_sym).blob.metadata[:width] > self.send(image_field.to_sym).blob.metadata[:height]
-      ActiveStorage::Variation.new(Uploads.resize_x_and_pad(width: width, height: height, blob: self.send(image_field.to_sym).blob))
-    else
-      ActiveStorage::Variation.new(Uploads.resize_to_fit(width: width, height: height, blob: self.send(image_field.to_sym).blob))
-    end if image_field.present?
+    end
   end
 end
