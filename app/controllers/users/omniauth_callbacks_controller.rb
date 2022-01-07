988665f9880792b22a57f943f134a301a516d976
Steven Ng <steven@nomadicoder.com>
2022-01-07 14:22:32 -0700

    Configure for omniauth and omniauth-google-oath2

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def google_oauth2
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.nil?
+      flash[:alert] = I18n.t "tupress.error.user_not_registered"
+      redirect_to "/"
+    elsif @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
+      sign_in_and_redirect @user, event: :authentication
+    else
+      flash[:alert] = I18n.t "manifold.error.user_not_registered"
+      redirect_to "/"
+    end
+  end
+end
