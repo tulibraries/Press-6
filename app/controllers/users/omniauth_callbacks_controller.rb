a1f6a6996bfda13dc1defac8d7c3081ac27e5e24
Steven Ng <steven@nomadicoder.com>
2022-01-18 11:41:21 -0700

    Restore en.yml and move devise error to devise.yml

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,13 +6,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.nil?
-      flash[:alert] = I18n.t "tupress.error.user_not_registered"
+      flash[:alert] = I18n.t "errors.messages.user_not_registered"
       redirect_to "/"
     elsif @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
       sign_in_and_redirect @user, event: :authentication
     else
-      flash[:alert] = I18n.t "manifold.error.user_not_registered"
+      flash[:alert] = I18n.t "errors.messages.user_not_registered"
       redirect_to "/"
     end
   end
