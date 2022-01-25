34e8b72a5f37372001a81c7c52271c223b23033e
Chris Doyle <chris@gcdoyle.com>
2022-01-25 12:39:47 -0500

    replace regex match for string include

--- spec/views/conferences/index.html.erb_spec.rb
+++ spec/views/conferences/index.html.erb_spec.rb
@@ -16,18 +16,18 @@ RSpec.describe "conferences/index.html.erb", type: :view do
 
   it "creates link to conference site" do
     render
-    expect(rendered).to match /href=\"#{conference1.link}/
+    expect(rendered).to include "href=\"#{conference1.link}
   end
 
   it "groups conferences by month" do
     render
-    expect(rendered).to match /<h2>#{conference1.start_date.strftime("%B")}/
-    expect(rendered).to match /<h2>#{conference2.start_date.strftime("%B")}/
+    expect(rendered).to include "<h2>#{conference1.start_date.strftime("%B")}"
+    expect(rendered).to include "<h2>#{conference2.start_date.strftime("%B")}"
   end
 
   it "renders top of page intro" do
     render
-    expect(rendered).to match /Hello World/
+    expect(rendered).to include "Hello World
   end
 
 end
