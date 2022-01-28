7b04ae2ffd41b04adee50b2ab0644c30b7b9cf2c
Chris Doyle <chris@gcdoyle.com>
2022-01-28 08:28:47 -0500

    sort highlights

--- app/controllers/webpages_controller.rb
+++ app/controllers/webpages_controller.rb
@@ -9,7 +9,7 @@ class WebpagesController < ApplicationController
     @news_events = Event.where(add_to_news: true).order(:created_at).take(1)
     @news_books = Book.where(add_to_news: true).order(:created_at).take(4)
     @news = @news_items + @news_events + @news_books
-    @highlights = Highlight.where(promote_to_homepage: true).sort
+    @highlights = Highlight.where(promote_to_homepage: true).order(:created_at).reverse
     @featured_catalog = Catalog.where.not(suppress: 1).order([year: :desc, season: :asc]).first
     @brochure = Brochure.where(promoted_to_homepage: 1).order(updated_at: :desc).first
     @philly = Subject.find_by(title: "Philadelphia Region")
