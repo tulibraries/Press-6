bda7d3706b4176717352e4561ae8d94f8e8ffb07
Chris Doyle <chris@gcdoyle.com>
2022-02-03 10:51:08 -0500

    update view for requiredness

--- app/webpack/javascripts/jumble.js
+++ app/webpack/javascripts/jumble.js
@@ -6,11 +6,27 @@ $(document).ready(function() {
       document.getElementById("examcopy").style.display = "block";
       document.getElementById("deskcopy").style.display = "none";
       document.getElementById("bookstore").style.display = "none";
+      document.getElementById("form_authorized_bookstore").classList.remove("required");
+      document.getElementById("form_authorized_bookstore").removeAttribute("required", "required");
+      document.getElementById("form_format_ebook").classList.remove("required");
+      document.getElementById("form_format_ebook").removeAttribute("required", "required");
+      document.getElementById("form_format_ebook").setAttribute("aria-required", "false");
+      document.getElementById("form_format_print").classList.remove("required");
+      document.getElementById("form_format_print").removeAttribute("required", "required");
+      document.getElementById("form_format_print").setAttribute("aria-required", "false");
     }
     if (this.value == "Desk Copy") {
       document.getElementById("examcopy").style.display = "none";
       document.getElementById("deskcopy").style.display = "block";
       document.getElementById("bookstore").style.display = "block";
+      document.getElementById("form_authorized_bookstore").classList.add("required");
+      document.getElementById("form_authorized_bookstore").setAttribute("required", "required");
+      document.getElementById("form_format_ebook").classList.add("required");
+      document.getElementById("form_format_ebook").setAttribute("required", "required");
+      document.getElementById("form_format_ebook").setAttribute("aria-required", "true");
+      document.getElementById("form_format_print").classList.add("required");
+      document.getElementById("form_format_print").setAttribute("required", "required");
+      document.getElementById("form_format_print").setAttribute("aria-required", "true");
     }
   });
 
