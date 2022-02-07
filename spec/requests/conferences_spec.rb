0e73ce24054dfb18a3b1a4a8d91c4ff50ea2d985
Chris Doyle <chris@gcdoyle.com>
2022-02-07 14:28:37 -0500

    rubocop

--- spec/requests/conferences_spec.rb
+++ spec/requests/conferences_spec.rb
@@ -3,7 +3,7 @@
  require "rails_helper"
 
  RSpec.describe "/conferences", type: :request do
-    Conference.destroy_all
+   Conference.destroy_all
    let(:future_conference) { FactoryBot.create(:conference, title: "future-conference") }
    let(:old_conference) { FactoryBot.create(:conference, title: "old-conference",
                                              start_date: DateTime.now.months_ago(3),
@@ -18,13 +18,13 @@
        expect { (get conferences_path).to be_successful }
      end
      it "returns a future conference" do
-      expect { get conferences_path.to have_text(future_conference.title) }
+       expect { get conferences_path.to have_text(future_conference.title) }
      end
      it "returns last month's conferences" do
-      expect { get conferences_path.to have_text(recent_conference.title) }
+       expect { get conferences_path.to have_text(recent_conference.title) }
      end
      it "does not return older than 1 month conference" do
-      expect { get conferences_path.to have_text(old_conference.title) }
+       expect { get conferences_path.to have_text(old_conference.title) }
      end
    end
  end
