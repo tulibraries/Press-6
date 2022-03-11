6f510ea68be1bf140600dab851d546fb1dde7567
Steven Ng <steven@nomadicoder.com>
2022-03-11 12:04:26 -0700

    look to see what's in tmp/xml

--- bin/xml-ingest.sh
+++ bin/xml-ingest.sh
@@ -1,3 +1,12 @@
 #! /usr/bin/env sh
+export KUBECONFIG="$HOME/.kube/prod-library.yaml"
+rancher context switch Tupress
+kubectl config set-context --current --namespace=tupress-prod
 PODID=`kubectl get pods | grep tupress-app | head -1 | cut -d' ' -f 1`
-kubectl exec $PODID ls
+
+# Copy local xml to tupress app
+kubectl cp $1 $PODID:tmp/xml 
+
+# Ingest
+#kubectl exec $PODID rails sync:pressworks:all[/app/tmp/xml]
+kubectl exec ls /app/tmp
