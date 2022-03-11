2af2ca489660ef75f191929d68c9a5bfabd92f19
Chris Doyle <chris@gcdoyle.com>
2022-03-11 14:35:12 -0500

    update xml-ingest.sh, add temp xmls for testing

--- bin/xml-ingest.sh
+++ bin/xml-ingest.sh
@@ -1,12 +1,22 @@
 #! /usr/bin/env sh
+set -e
 export KUBECONFIG="$HOME/.kube/prod-library.yaml"
 rancher context switch Tupress
 kubectl config set-context --current --namespace=tupress-prod
 PODID=`kubectl get pods | grep tupress-app | head -1 | cut -d' ' -f 1`
 
-# Copy local xml to tupress app
-kubectl cp $1 $PODID:tmp/xml 
+ingest() {
+  # Copy local xml to tupress app
+  kubectl cp $1 $PODID:tmp/xml 
 
-# Ingest
-#kubectl exec $PODID rails sync:pressworks:all[/app/tmp/xml]
-kubectl exec ls /app/tmp
+  # Ingest
+  #kubectl exec $PODID rails sync:pressworks:all[/app/tmp/tupress-delta.xml]
+  kubectl exec $PODID ls /app/tmp
+}
+
+if [ -d $1 ]; then
+  find $1 -name *.xml -exec ingest {} \;
+else
+  ingest $1
+fi
+
