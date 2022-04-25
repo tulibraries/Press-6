64730af1f15b5829b8297aaaece4c0b91ad5cd27
Chris Doyle <chris@gcdoyle.com>
2022-04-25 11:24:07 -0400

    update migration

--- db/migrate/20220303135111_add_date_sort_fields_to_book.rb
+++ db/migrate/20220303135111_add_date_sort_fields_to_book.rb
@@ -2,7 +2,7 @@
 
 class AddDateSortFieldsToBook < ActiveRecord::Migration[6.1]
   def change
-    def change_table :books, bulk: true do |t|
+    change_table :books, bulk: true do |t|
       add_column :books, :sort_year, :string
       add_column :books, :sort_month, :string
     end
