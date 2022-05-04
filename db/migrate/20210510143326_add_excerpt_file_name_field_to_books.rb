d967d1e620423759e68222b53aea222e88c64254
Chris Doyle <chris@gcdoyle.com>
2022-05-04 09:21:35 -0400

    Run migrations with fix to generate new schema. Remove migrations. Add new migration to check last timestamp in local db is not less than new REQUIRED_VERSION so that migration fixes are applied universally

--- db/migrate/20210510143326_add_excerpt_file_name_field_to_books.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddExcerptFileNameFieldToBooks < ActiveRecord::Migration[6.0]
-  def change
-    add_column :books, :excerpt_file_name, :string
-  end
-end
--- db/migrate/20220201192914_add_label_field_for_q_as.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddLabelFieldForQAs < ActiveRecord::Migration[6.1]
-  if column_exists?(:books, :toc_label)
-    rename_column :books, :toc_label, :qa_label
-  end
-end
