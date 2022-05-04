3761c10388f0ad84c81edd7164d9af0f9f788379
Chris Doyle <chris@gcdoyle.com>
2022-05-04 11:38:26 -0400

    make migration reversible

--- db/migrate/20220503123359_update_active_storage_forign_key_type.rb
+++ db/migrate/20220503123359_update_active_storage_forign_key_type.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class UpdateActiveStorageForeignKeyType < ActiveRecord::Migration[6.1]
-  def change
+  def up
     change_column :active_storage_variant_records, :blob_id, :bigint
+  end
+  def down
+    change_column :active_storage_variant_records, :blob_id, :int
   end
 end
