afa87b5523ae5fad709bc14f2da9c7acb5d6d74e
Chris Doyle <chris@gcdoyle.com>
2022-05-04 17:18:37 -0400

    so close

--- db/migrate/20220503123358_create_structure.rb
+++ db/migrate/20220503123358_create_structure.rb
@@ -394,6 +394,7 @@ class CreateStructure < ActiveRecord::Migration[6.1]
     add_index :action_text_rich_texts, %i[record_type record_id name], name: "index_action_text_rich_texts_uniqueness", unique: true
     add_index :active_storage_attachments, %i[record_type record_id name blob_id], name: "index_active_storage_attachments_uniqueness", unique: true
     add_index :active_storage_blobs, [:key], unique: true
+
     change_table :books, bulk: true do |t|
       t.index :series_id
       t.index :catalog_id
@@ -410,9 +411,11 @@ class CreateStructure < ActiveRecord::Migration[6.1]
       t.index :brochure_id, unique: true
       t.index :subject_id, unique: true
     end
-    add_index :friendly_id_slugs, [:sluggable_type, :sluggable_id]
-    add_index :friendly_id_slugs, [:slug, :sluggable_type], length: { slug: 140, sluggable_type: 50 }
-    add_index :friendly_id_slugs, [:slug, :sluggable_type, :scope], length: { slug: 70, sluggable_type: 50, scope: 70 }, unique: true
+    change_table :friendly_id_slugs, bulk: true do |t|
+      t.index [:sluggable_type, :sluggable_id]
+      t.index [:slug, :sluggable_type], length: { slug: 140, sluggable_type: 50 }
+      t.index [:slug, :sluggable_type, :scope], length: { slug: 70, sluggable_type: 50, scope: 70 }, unique: true
+    end
 
     add_index :agencies, :slug, unique: true
     add_index :authors, :slug, unique: true
