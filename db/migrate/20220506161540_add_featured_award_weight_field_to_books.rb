b05f3c862fce8dc66e63fa4e3bde3fe2778a87ca
Chris Doyle <chris@gcdoyle.com>
2022-05-06 12:23:09 -0400

    add column to table.

---
+++ db/migrate/20220506161540_add_featured_award_weight_field_to_books.rb
@@ -0,0 +1,9 @@
+class AddFeaturedAwardWeightFieldToBooks < ActiveRecord::Migration[6.1]
+  def change
+    def change
+      unless column_exists?(:books, :featured_award_weight)
+        add_column :books, :featured_award_weight, :int
+      end
+    end
+  end
+end
