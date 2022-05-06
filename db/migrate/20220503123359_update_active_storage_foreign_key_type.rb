9777a34f0717a85b280e6fa2272c0223f310ef11
Chris Doyle <cdoyle@temple.edu>
2022-05-06 10:18:38 -0400

    remove potentially destructive migration (#361)
    * remove potentially destructive migration
    
    * update new migration to only run if doesn't exist.
    
    Co-authored-by: Chris Doyle <chris@gcdoyle.com>

--- db/migrate/20220503123359_update_active_storage_foreign_key_type.rb
+++ db/migrate/20220503123359_update_active_storage_foreign_key_type.rb
@@ -2,11 +2,13 @@
 
 class UpdateActiveStorageForeignKeyType < ActiveRecord::Migration[6.1]
   def up
-    remove_column :active_storage_variant_records, :blob_id
-    add_belongs_to :active_storage_variant_records, :blob, null: false, index: false, type: :bigint
+    unless column_exists?(:active_storage_variant_records, :blob_id, type: :bigint)
+      change_column :active_storage_variant_records, :blob_id, :bigint
+    end
   end
   def down
-    remove_column :active_storage_variant_records, :blob_id
-    add_belongs_to :active_storage_variant_records, :blob, null: false, index: false
+    if column_exists?(:active_storage_variant_records, :blob_id, type: :bigint)
+      change_column :active_storage_variant_records, :blob_id, :integer
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,11 +45,11 @@ ActiveRecord::Schema.define(version: 2022_05_04_135058) do
   end
 
   create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "blob_id", null: false
-    t.index ["variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "agencies", charset: "utf8mb3", force: :cascade do |t|
@@ -420,6 +420,7 @@ ActiveRecord::Schema.define(version: 2022_05_04_135058) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "books", "special_offers"
   add_foreign_key "brochures", "catalogs"
   add_foreign_key "brochures", "subjects"
