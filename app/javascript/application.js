5f0217b7e702d6098c8e1af2a019e26dfc72c3fc
Chris Doyle <cdoyle@temple.edu>
2022-06-07 16:04:10 -0400

    finish trix, start selectize

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,12 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./src/jquery"
+// import "sifter"
+// import "microplugin"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "trix"
 import "@rails/actiontext"
 
-import "./src/selectize.min"
-import "./src/selectize.bootstrap5.min"
+// import "./src/selectize"
+// import "./src/selectize.bootstrap5.min"
