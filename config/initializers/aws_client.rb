3f4561bee18402bf208ff72a10a8c63838409387
David Kinzer <dtkinzer@gmail.com>
2022-06-08 16:13:24 -0400

    Alwasy add aws client credentials.

--- config/initializers/aws_client.rb
+++ config/initializers/aws_client.rb
@@ -2,12 +2,10 @@
 
 require "aws-sdk-s3"
 
-if Rails.configuration.active_storage.service == :amazon
-  key = Rails.application.credentials.dig(:aws, :access_key_id)
-  secret = Rails.application.credentials.dig(:aws, :secret_access_key)
+key = Rails.application.credentials.dig(:aws, :access_key_id)
+secret = Rails.application.credentials.dig(:aws, :secret_access_key)
 
-  Aws.config.update(
-    region: "us-east-1",
-    credentials: Aws::Credentials.new(key, secret)
-  )
-end
+Aws.config.update(
+  region: "us-east-1",
+  credentials: Aws::Credentials.new(key, secret)
+)
