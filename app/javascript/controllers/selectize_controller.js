5380800a08bc98b7faa6e8aa195b5c0447d8e859
Chris Doyle <cdoyle@temple.edu>
2022-06-10 06:48:52 -0400

    clean up controller

--- app/javascript/controllers/selectize_controller.js
+++ app/javascript/controllers/selectize_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["entries", "pagination"]
   connect() {
     $("select").selectize();
     $("select.limited").selectize({
