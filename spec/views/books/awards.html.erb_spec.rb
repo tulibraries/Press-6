256997a6699fbd23563b4fd1a6e9e65c8607c7af
Chris Doyle <cdoyle@temple.edu>
2022-07-11 16:19:18 -0400

    rubocop

---
+++ spec/views/books/awards.html.erb_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "books/show", type: :view do
+  let(:user) { FactoryBot.create(:user) }
+
+  before(:each) do
+    allow(view).to receive(:current_user).and_return(user)
+  end
+
+  it "populates expected instance variables" do
+    @book = FactoryBot.create(:book)
+    assign(:links, [[@book.label_1, @book.link_1]])
+    render
+    expect(rendered).to match(/#{@book.label_1}/)
+  end
+
+  it "uses helper to add century to pub_dates" do
+    @book = FactoryBot.create(:book,
+                              bindings: '{"binding":[{"format":"PB","price":"$31.95","ean":"978-1-59213-386-4","binding_status":"IP","pub_date_for_format":"Jun 85"}]}')
+    assign(:links, [[@book.label_1, @book.link_1]])
+    render
+    expect(rendered).to match(/1985/)
+  end
+
+  it "renders subjects from awards instance variable" do
+    @book = FactoryBot.create(:book)
+    assign(:awards_by_subject, [@book.subject1])
+    expect(rendered).to match(/#{@book.subject1}/)
+  end
+end
