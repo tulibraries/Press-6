bdc63efb073fbe7127d2e2e1b1ae2678fc02749b
Chris Doyle <cdoyle@temple.edu>
2022-07-12 09:51:31 -0400

    rubocop

--- db/migrate/20220712133028_change_catalog_suppress_field_default.rb
+++ db/migrate/20220712133028_change_catalog_suppress_field_default.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 class ChangeCatalogSuppressFieldDefault < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :catalogs, :suppress, :boolean, default: true
+  end
+  def down
+    remove_column :catalogs, :suppress
   end
 end
