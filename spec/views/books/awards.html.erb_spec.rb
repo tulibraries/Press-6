688f4e5bd5f7d508e09f9154903650cf906a4900
Chris Doyle <cdoyle@temple.edu>
2022-07-12 09:48:49 -0400

    adjust test and partials

--- spec/views/books/awards.html.erb_spec.rb
+++ spec/views/books/awards.html.erb_spec.rb
@@ -2,31 +2,19 @@
 
 require "rails_helper"
 
-RSpec.describe "books/show", type: :view do
+RSpec.describe "books/awards", type: :view do
   let(:user) { FactoryBot.create(:user) }
 
   before(:each) do
     allow(view).to receive(:current_user).and_return(user)
   end
 
-  it "populates expected instance variables" do
-    @book = FactoryBot.create(:book)
-    assign(:links, [[@book.label_1, @book.link_1]])
-    render
-    expect(rendered).to match(/#{@book.label_1}/)
-  end
-
-  it "uses helper to add century to pub_dates" do
-    @book = FactoryBot.create(:book,
-                              bindings: '{"binding":[{"format":"PB","price":"$31.95","ean":"978-1-59213-386-4","binding_status":"IP","pub_date_for_format":"Jun 85"}]}')
-    assign(:links, [[@book.label_1, @book.link_1]])
-    render
-    expect(rendered).to match(/1985/)
-  end
-
   it "renders subjects from awards instance variable" do
     @book = FactoryBot.create(:book)
-    assign(:awards_by_subject, [@book.subject1])
-    expect(rendered).to match(/#{@book.subject1}/)
+    subject = FactoryBot.create(:subject)
+    assign(:awards_by_subject, [subject])
+    render
+    # binding.pry
+    expect(rendered).to have_text(/#{subject.title}/)
   end
 end
