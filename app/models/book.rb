16b0aa57676886f6aa31ae19f58be68972e3f4fb
Chris Doyle <cdoyle@temple.edu>
2022-07-18 14:52:26 -0400

    add displayable filter to search results

--- app/models/book.rb
+++ app/models/book.rb
@@ -37,10 +37,10 @@ class Book < ApplicationRecord
 
   belongs_to :series, optional: true
 
-  scope :displayable, -> { where(suppress_from_view: false).where(status: %w[NP IP]) }
+  scope :displayable, -> { where(suppress_from_view: [nil, false]).where(status: %w[NP IP]) }
 
   scope :requestable, -> {
-    .where("bindings LIKE ?", '%"format":"PB"%')
+    where("bindings LIKE ?", '%"format":"PB"%')
     .where(desk_copy: [nil, false])
   }
 
