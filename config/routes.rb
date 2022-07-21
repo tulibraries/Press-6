cb62295684e434354b613c41627e48d18053858d
Chris Doyle <cdoyle@temple.edu>
2022-07-21 12:08:45 -0400

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" } 
-  
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destroy"
   end
 
   concern :imageable do
