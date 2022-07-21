5d8ed34e0e46439b0c71d545e81fd0e6363242ca
Chris Doyle <cdoyle@temple.edu>
2022-07-21 11:32:14 -0400

    add logout link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" } 
+  
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
 
   concern :imageable do
     get "image/thumbnail", to: "images#thumbnail_image"
