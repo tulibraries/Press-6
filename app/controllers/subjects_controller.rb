e6e262fccb914e95987179f0178617d8d4d20a18
Chris Doyle <cdoyle@temple.edu>
2022-08-11 15:20:11 -0400

    rubocop

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -10,16 +10,16 @@ class SubjectsController < ApplicationController
 
   def show
     sort = params[:sort]
-    @books = if sort.present? && sort == "year"
-              Book.displayable
-                  .where("subjects LIKE ?", "%#{@subject.code}%")
-                  .order("sort_year DESC")
-                  .page params[:page]
-            else
-              Book.displayable
-                  .where("subjects LIKE ?", "%#{@subject.code}%")
-                  .order(:sort_title)
-                  .page params[:page]
+    @books =  if sort.present? && sort == "year"
+                Book.displayable
+                    .where("subjects LIKE ?", "%#{@subject.code}%")
+                    .order("sort_year DESC")
+                    .page params[:page]
+              else
+                Book.displayable
+                    .where("subjects LIKE ?", "%#{@subject.code}%")
+                    .order(:sort_title)
+                    .page params[:page]
               end
     @brochures = @subject.brochures
   end
