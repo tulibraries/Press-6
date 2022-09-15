c895ea9600764a32e61cfed44be2dfff7a3d8573
Chris Doyle <cdoyle@temple.edu>
2022-09-15 11:40:31 -0400

    move analytics script from inline

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,3 +13,17 @@ import "./src/jumble"
 import "./src/administrate-trix"
 
 
+var _gaq = _gaq || [];
+
+function loadtracking() {
+  window._gaq.push(['_setAccount', 'UA-2152985-17']);
+  window._gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/analytics.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+}
+
+loadtracking();
