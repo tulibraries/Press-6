0d3eb3592df52aa2bf99d8f36693f99091fc3ca6
Chris Doyle <cdoyle@temple.edu>
2022-09-16 14:01:30 -0400

    move analytics scripts back inline, using nonce for security

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,19 +11,3 @@ import "@rails/actiontext"
 import "./src/selectize"
 import "./src/jumble"
 import "./src/administrate-trix"
-
-
-var _gaq = _gaq || [];
-
-function loadtracking() {
-  _gaq.push(['_setAccount', 'UA-2152985-17']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/analytics.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-}
-
-loadtracking();
