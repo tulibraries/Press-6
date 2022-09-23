3368bbe6c99bdea54e195944d28d638a85c56ae6
Chris Doyle <cdoyle@temple.edu>
2022-09-23 09:09:02 -0400

    make fields required

--- spec/requests/forms/rights_permissions_spec.rb
+++ spec/requests/forms/rights_permissions_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe "Rights and Permissions", type: :request do
   let(:form_type) { "rights-and-permissions" }
   let(:form_params) do
     {
-      name: "Joe", email: "test@temple.edu", address_line_1: "123 Hammock Sway", state: "FL", zip: "345321"
-
+      name: "Joe", email: "test@temple.edu", address_line_1: "123 Hammock Sway", state: "FL", zip: "345321",
+      book_title: "Title", book_author_editor: "Angela", chapter_title: "Chapter One", page_numbers: "3, 4", 
+      your_publisher: "Knopf Press", reprint_title: "Second Edition", number_of_pages: "344", publication_date: "09/22/2022", 
+      rights_requested: "All inclusive" 
     }
   end
 
