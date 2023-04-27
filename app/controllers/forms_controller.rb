23a9c74d1bdb1d84e8fbf777ead79a908780d542
Chris Doyle <cdoyle@temple.edu>
2023-04-27 08:54:49 -0400

    add error message support

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,20 +1,10 @@
 # frozen_string_literal: true
 
 class FormsController < ApplicationController
-  before_action :html_check, only: :create
-
-  def html_check
-    binding.pry
-    if params[:form][:comments].present?
-      if params[:form][:comments].include? "<"
-        flash[:alert] = "HTML markup is not allowed in comments."
-        render :create, denied: true
-      end
-    end
-  end
 
   def new
-    @form = Form.new
+    @form = (params[:form].present? ? Form.new(params[:form]) : Form.new)
+    @notice = flash.now[:notice] if :notice.present?
     if existing_forms.include? params[:type]
       @type = params[:type]
       existing_forms.each do |form|
@@ -23,7 +13,7 @@ class FormsController < ApplicationController
       end
       @books = Book.displayable.requestable.order(:sort_title)
       @book = Book.find(params[:id]) if params[:id].present?
-      render template: "forms/create"
+      create if params[:form].present?
     else
       render "errors/not_found", status: :not_found
     end
@@ -35,15 +25,33 @@ class FormsController < ApplicationController
     @type = params[:form][:form_type]
 
     if verify_recaptcha(model: @form)
-      if @form.deliver
-        redirect_to root_path(@form), notice: "Thank you for your message. We will contact you soon!"
+      if params[:form][:comments].include? "<"
+        failure("html")
       else
-        redirect_to new_form_path(type: @type), notice: "Cannot send message."
+        @form.deliver ? success : failure("mail")
       end
     else
-      render :create, form: params[:form]
-      flash[:alert] = "Please prove you are human."
+      failure("recaptcha")
     end
+  end
+
+  def success
+    redirect_to root_path, notice: "Thank you for your message. We will contact you soon!"
+  end
+
+  def failure(mode)
+    case mode
+    when "html"
+      notice = "HTML markup is not allowed in comments."
+      flash.now[:notice] = notice
+    when "recaptcha"
+      notice = "Please prove you are human."
+      flash.now[:notice] = notice
+    when "mail"
+      notice = "Unable to send form."
+      flash.now[:notice] = notice
+    end
+    render :new, notice: notice
   end
 
   def existing_forms
