e22bb91d78e2dd8bbdf9fed006f1293391e98c40
Chris Doyle <cdoyle@temple.edu>
2023-11-20 11:36:12 -0500

    update failing tests

--- spec/views/conferences/index.html.erb_spec.rb
+++ spec/views/conferences/index.html.erb_spec.rb
@@ -5,7 +5,8 @@ require "rails_helper"
 RSpec.describe "conferences/index", type: :view do
   let(:conference1) { FactoryBot.create(:conference, start_date: DateTime.now) }
   let(:conference2) { FactoryBot.create(:conference, start_date: DateTime.now.next_month) }
-  let(:intro) { FactoryBot.create(:webpage, :with_text) }
+  # binding.pry
+  let(:intro) { FactoryBot.create(:webpage, body: ActionText::Content.new("Hello World")) }
   let(:user) { FactoryBot.create(:user) }
 
   before(:each) do
