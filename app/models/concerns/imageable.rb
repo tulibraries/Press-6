af743511fbabe0b8325fa0dcb4e42e55d440633e
Chris Doyle <cdoyle@temple.edu>
2023-12-14 10:13:27 -0500

    update image analysis check

--- app/models/concerns/imageable.rb
+++ app/models/concerns/imageable.rb
@@ -17,11 +17,10 @@ module Imageable
 
   def custom_image(image_field, width, height)
     image = send(image_field.to_sym)
-    if image.present?
-      image.analyze unless image.analyzed
+    image.analyze unless image.analyzed?
 
-      image_width = image.metadata[:width]
-      image_height = image.metadata[:height]
+    image_width = image.metadata[:width]
+    image_height = image.metadata[:height]
 
       if (image_width != width) || (image_height != height)
         if image_width > image_height
@@ -39,6 +38,5 @@ module Imageable
       else
         image
       end
-    end
   end
 end
