cac0cc1da21b2b41e62e69f712d991a45a98c3da
Chris Doyle <cdoyle@temple.edu>
2023-12-18 09:48:53 -0500

    Update people_spec.rb

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe "/people", type: :feature do
   let!(:default_person) { FactoryBot.create(:person, :with_image, title: "Lemon Head", department: "Human Resources", head: false) }
   let!(:person) { FactoryBot.create(:person, :with_image) }
 
+  it "analyzes images" do
+    expect(person.image.analyzed).to be
+    expect(default_person.image.analyzed).to be
+    expect(dept_head.image.analyzed).to be
+  end
+
   it "displays all people" do
     visit people_path
     container = page.find("#people")
