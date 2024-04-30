538d0a1f193ff1e75a1e3bfdc01605b26fd2a657
Chris Doyle <cdoyle@temple.edu>
2024-04-30 16:28:48 -0400

    add phone field to review-copy form (#945)

--- spec/requests/forms/review_copy_spec.rb
+++ spec/requests/forms/review_copy_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Request a Review Copy", type: :request do
   let(:form_type) { "review-copy" }
   let(:form_params) do
     {
-      name: "Joe", email: "test@temple.edu", address_line_1: "123 Hammock Sway", state: "FL", zip: "345321", media_type: "Magazine",
+      name: "Joe", email: "test@temple.edu", phone: "215-204-1286", address_line_1: "123 Hammock Sway", state: "FL", zip: "345321", media_type: "Magazine",
       comments: "The sun WILL come out, tomorrow", book_title: "Little Orphan Annie"
     }
   end
