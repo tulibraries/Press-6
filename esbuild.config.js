a4145449c72499290545062d82ada0cc88c870be
sensei100 <tuh42082@temple.edu>
2024-05-15 11:38:33 -0400

    Use context instead of build

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const rails = require('esbuild-rails')
 
-require("esbuild").build({
+require("esbuild").context({
   entryPoints: ["application.js"],
   bundle: true,
   outdir: path.join(process.cwd(), "app/assets/builds"),
