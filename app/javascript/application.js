522ff743d93e55833a00584af86f5762c6490de5
Chris Doyle <cdoyle@temple.edu>
2024-08-19 08:47:37 -0400

    re-add jquery using src folder method: console error where $ sign was not recognized.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import { DirectUpload } from "@rails/activestorage"
-import "jquery"
 import "sifter"
 import "microplugin"
 import "./controllers"
@@ -9,5 +8,6 @@ import * as bootstrap from "bootstrap"
 import Trix from "trix";
 import "@rails/actiontext"
 import "selectize"
+import "./src/jquery"
 import "./src/jumble"
 import "./src/administrate-trix"
