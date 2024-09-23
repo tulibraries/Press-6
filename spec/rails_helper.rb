d15cbf0acc232ce73ca5cd15634c5d57d1b1bdd4
Chris Doyle <cdoyle@temple.edu>
2024-09-23 15:26:39 -0400

    move test to request spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,10 +45,6 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :helper
   config.include Devise::Test::ControllerHelpers, type: :view
 
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome_headless
-  end
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = Rails.root.join("spec/fixtures")
 
