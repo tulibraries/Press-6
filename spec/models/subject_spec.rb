df6d84c4e9ba8764c7d7cbde5aff0ddd087548ae
sensei100 <tuh42082@temple.edu>
2024-09-27 13:33:54 -0400

    TWM-517 Fix Honeybadger ActiveRecord::StatementInvalid: PG::InvalidRegularExpression: ERROR

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe Subject, type: :model do
 
   it_behaves_like "attachable"
   it_behaves_like "detachable"
+
+  describe ".search(q)" do
+    let!(:subject1) { Subject.create!(title: "Ruby on Rails Guide") }
+    let!(:subject2) { Subject.create!(title: "PostgreSQL for Beginners") }
+    let!(:subject3) { Subject.create!(title: "C++ Programming") }
+
+    it "returns subjects that match the results" do
+      results = Subject.search("Rails")
+      expect(results).to include(subject1)
+      expect(results).not_to include(subject2)
+    end
+
+    it "is case insensitive" do
+      results = Subject.search("ruby")
+      expect(results).to include(subject1)
+    end
+
+    it "escapes special regex characters" do
+      results = Subject.search("C++")
+      expect(results).to include(subject3)
+    end
+  end
 end
