59cf1c7d4ad1fd6c83bf58a9810bccad96fb9d00
Chris Doyle <cdoyle@temple.edu>
2024-10-15 16:16:17 -0400

    add selenium options to capybara config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,20 @@ require "rspec/rails"
 require "support/factory_bot"
 require "capybara/rails"
 
+Capybara.register_driver :selenium do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument("--headless")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--window-size=1366,720")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+end
+
+Capybara.javascript_driver = :selenium
+
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 include Warden::Test::Helpers
