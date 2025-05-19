793b9d489966e566cbe6adda8b963dcc2f5d199f
Chris Doyle <cdoyle@temple.edu>
2025-05-19 11:45:41 -0400

    add image alt text field to events model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_19_150256) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_19_152202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -248,6 +248,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_19_150256) do
     t.boolean "add_to_news"
     t.string "slug", limit: 255
     t.bigint "news_weight"
+    t.string "image_alt_text"
     t.index ["slug"], name: "idx_19197_index_events_on_slug", unique: true
   end
 
