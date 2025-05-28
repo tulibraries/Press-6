292d880e389aeb5b1399140e3dccb8cb4471ba88
sensei100 <tuh42082@temple.edu>
2025-05-28 13:49:59 -0400

    Skip checking the . directory

--- config/initializers/tmpdir.rb
+++ config/initializers/tmpdir.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
-# Only run this initializer in runtime contexts
+require "tmpdir"
 
-unless ENV["SKIP_TMPDIR_INIT"]
-  require "tmpdir"
-
-  if ENV["TMPDIR"].nil? || !File.writable?(ENV["TMPDIR"])
-    raise "TMPDIR is not set or is not writable: #{ENV['TMPDIR'].inspect}"
+module Dir
+  class << self
+    def tmpdir
+      env_tmp = ENV["TMPDIR"]
+      if env_tmp && File.directory?(env_tmp) && File.writable?(env_tmp)
+        return env_tmp
+      end
+      # Skip checking `.` which is `/app` and not writable
+      ["/app/tmp", "/tmp", "/var/tmp"].each do |path|
+        return path if File.directory?(path) && File.writable?(path)
+      end
+      raise ArgumentError, "could not find a writable temporary directory"
+    end
   end
-
-  Rails.logger.info "Using TMPDIR: #{ENV['TMPDIR']}" if defined?(Rails)
 end
+
+
