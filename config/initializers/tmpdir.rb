ca5b9a3586618581387d77c87b7b975c7465a2ea
sensei100 <tuh42082@temple.edu>
2025-05-28 12:14:49 -0400

    More fixes

--- config/initializers/tmpdir.rb
+++ config/initializers/tmpdir.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Only run this initializer in runtime contexts
 
 unless ENV["SKIP_TMPDIR_INIT"]
@@ -8,5 +9,5 @@ unless ENV["SKIP_TMPDIR_INIT"]
     raise "TMPDIR is not set or is not writable: #{ENV['TMPDIR'].inspect}"
   end
 
- Rails.logger.info "Using TMPDIR: #{ENV['TMPDIR']}" if defined?(Rails)
+  Rails.logger.info "Using TMPDIR: #{ENV['TMPDIR']}" if defined?(Rails)
 end
