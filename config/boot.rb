38f0d96929126d3824af4e61c4f74d92ddc0eba5
sensei100 <tuh42082@temple.edu>
2025-05-28 15:26:13 -0400

    Fix env var

--- config/boot.rb
+++ config/boot.rb
@@ -5,5 +5,5 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 
-ENV["TMPDIR"] = "/app/tmp"
+ENV["TMPDIR"] = "/mnt/tmp"
 require_relative "tmpdir_patch"
