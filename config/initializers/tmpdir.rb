655577d9b8ba1bcbb3abd99f0f4c86be61c7a28b
sensei100 <tuh42082@temple.edu>
2025-05-28 13:59:32 -0400

    Change config

--- config/initializers/tmpdir.rb
+++ config/initializers/tmpdir.rb
@@ -2,18 +2,24 @@
 
 require "tmpdir"
 
-module Dir
-  class << self
-    def tmpdir
-      env_tmp = ENV["TMPDIR"]
-      if env_tmp && File.directory?(env_tmp) && File.writable?(env_tmp)
-        return env_tmp
-      end
-      # Skip checking `.` which is `/app` and not writable
-      ["/app/tmp", "/tmp", "/var/tmp"].each do |path|
-        return path if File.directory?(path) && File.writable?(path)
-      end
-      raise ArgumentError, "could not find a writable temporary directory"
+# config/initializers/tmpdir.rb
+
+require "tmpdir"
+
+class << Dir
+  def tmpdir
+    env_tmp = ENV["TMPDIR"]
+    if env_tmp && File.directory?(env_tmp) && File.writable?(env_tmp)
+      return env_tmp
     end
+
+    # Intentionally skip checking `.` (Dir.pwd) because in containerized environments,
+    # the working directory (e.g., /app) may be read-only, which will cause Ruby to raise
+    # an ArgumentError during temp file creation even if TMPDIR is correctly set.
+    ["/app/tmp", "/tmp", "/var/tmp"].each do |path|
+      return path if File.directory?(path) && File.writable?(path)
+    end
+
+    raise ArgumentError, "could not find a writable temporary directory"
   end
 end
