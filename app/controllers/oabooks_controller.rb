2a16220c305549f0b60e6c2cefa0871610dc6259
Chris Doyle <cdoyle@temple.edu>
2025-10-06 09:55:35 -0400

    switch to find by slug, add tests

--- app/controllers/oabooks_controller.rb
+++ app/controllers/oabooks_controller.rb
@@ -8,12 +8,12 @@ class OabooksController < ApplicationController
 
   def north_broad_press
     @oabooks = Oabook.where(collection: "North Broad Press").order(:title)
-    @page = Webpage.find_by(title: "North Broad Press Intro")
+    @page = Webpage.find_by(slug: "north-broad-press-intro")
   end
 
   def labor_studies
     @oabooks = Oabook.where(collection: "Labor Studies & Work").order(:title)
-    @page = Webpage.find_by(title: "Labor Studies & Work Intro")
+    @page = Webpage.find_by(slug: "labor-studies-work-intro")
   end
 
   private
