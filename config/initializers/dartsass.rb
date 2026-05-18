f19607b3b8fe38cac0c5b21546539bcaab9b6f6c
Chris Doyle <cdoyle@temple.edu>
2026-05-18 10:45:54 -0400

    rename scss file for dartsasss build step

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   bootstrap_stylesheets = File.join(Gem.loaded_specs.fetch("bootstrap").full_gem_path, "assets/stylesheets")
 
   config.dartsass.builds = {
-    "application.css.scss" => "application.css",
+    "application.scss" => "application.css",
     "administrate/application.scss" => "administrate/application.css"
   }
 
