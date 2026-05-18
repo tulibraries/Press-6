66c71f43d6357a0ac3744ef5966377e05dd9d2e3
Chris Doyle <cdoyle@temple.edu>
2026-05-18 12:18:49 -0400

    rubocop

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
- Rails.application.config.session_store :cookie_store, key: "_tupress_session", expire_after: 1.day
+Rails.application.config.session_store :cookie_store, key: "_tupress_session", expire_after: 1.day
